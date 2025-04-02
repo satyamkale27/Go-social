b000fb3cdee60ceae75f9ad38f738b401e8e4aa0
satyam <satyamkale271@gmail.com>
2025-04-02 17:53:43 +0530

    database connection

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -16,6 +16,13 @@ type application struct {
 
 type config struct {
 	addr string
+	db   dbConfig
+}
+type dbConfig struct {
+	addr         string
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  string
 }
 
 func (app *application) mount() http.Handler {
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,12 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", ":postgress://user:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("MAX_IDLE_TIME", "15min"),
+		},
 	}
 
 	store := store2.NewStorage(nil)
