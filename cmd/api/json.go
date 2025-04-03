a1ca4396b966b4644cf3727ceab7a25db2933448
satyam <satyamkale271@gmail.com>
2025-04-03 21:04:09 +0530

    maxbytes set to 1mb

--- cmd/api/json.go
+++ cmd/api/json.go
@@ -11,3 +11,11 @@ func writeJSON(w http.ResponseWriter, status int, data any) error {
 	return json.NewEncoder(w).Encode(data)
 
 }
+
+func readJSON(w http.ResponseWriter, r *http.Request, data any) error {
+
+	maxBytes := 1_048_573
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	decoder := json.NewDecoder(r.Body)
+	return decoder.Decode(data)
+}
