d6e69ca2b8d9a415e0e9cfa3a383ee16d27bf2b9
satyam <satyamkale271@gmail.com>
2025-04-07 17:30:15 +0530

    writejsonerror replaced witl custon error methods

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -6,7 +6,19 @@ import (
 )
 
 func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
-	log.Printf("internal error: %s path: %s", r.Method, r.URL.Path, err)
+	log.Printf("internal error: %s path: %s error: %s", r.Method, r.URL.Path, err)
 	writeJSONError(w, http.StatusInternalServerError, "the server encountered a problem")
+
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("bad request error: %s path: %s error: %s", r.Method, r.URL.Path, err)
+	writeJSONError(w, http.StatusBadRequest, err.Error())
+
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("not found error: %s path: %s error: %s", r.Method, r.URL.Path, err)
+	writeJSONError(w, http.StatusNotFound, "not found")
 
 }
