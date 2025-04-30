8c124f5475fd134b5a5e9202a54eef8f1a52da65
satyam <satyamkale271@gmail.com>
2025-04-30 17:30:34 +0530

    error.error

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -5,25 +5,25 @@ import (
 )
 
 func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
-	app.logger.Errorw("internal  error", "method", r.Method, "path", r.URL.Path, "error", err)
+	app.logger.Errorw("internal  error", "method", r.Method, "path", r.URL.Path, "error", err.Error())
 	writeJSONError(w, http.StatusInternalServerError, "the server encountered a problem")
 
 }
 
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
-	app.logger.Warnf("bad request", "method", r.Method, "path", r.URL.Path, "error", err)
+	app.logger.Warnf("bad request", "method", r.Method, "path", r.URL.Path, "error", err.Error())
 	writeJSONError(w, http.StatusBadRequest, err.Error())
 
 }
 
 func (app *application) conflictResponce(w http.ResponseWriter, r *http.Request, err error) {
-	app.logger.Errorw("conflict response", "method", r.Method, "path", r.URL.Path, "error", err)
+	app.logger.Errorw("conflict response", "method", r.Method, "path", r.URL.Path, "error", err.Error())
 	writeJSONError(w, http.StatusConflict, err.Error())
 
 }
 
 func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
-	app.logger.Warnf("not found error", "method", r.Method, "path", r.URL.Path, "error", err)
+	app.logger.Warnf("not found error", "method", r.Method, "path", r.URL.Path, "error", err.Error())
 	writeJSONError(w, http.StatusNotFound, "not found")
 
 }
