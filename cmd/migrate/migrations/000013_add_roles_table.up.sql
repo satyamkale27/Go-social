b705c4971e55e7c0fb5df25eb4a81c4fd94ae1d5
satyam <satyamkale271@gmail.com>
2025-05-15 20:25:23 +0530

    users_roles migration

--- cmd/migrate/migrations/000013_add_roles_table.up.sql
+++ cmd/migrate/migrations/000013_add_roles_table.up.sql
@@ -1,31 +1,30 @@
 CREATE TABLE IF NOT EXISTS roles (
-
-    id BiGSERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE,
-    level int NOT NULL DEFAULT 0,
+                                     id BIGSERIAL PRIMARY KEY,
+                                     name VARCHAR(255) NOT NULL UNIQUE,
+    level INT NOT NULL DEFAULT 0,
     description TEXT
-);
-
-INSERT INTO
-  roles (name, description, level)
-VALUE (
-    'user',
-     'A user can create post and comments',
-    1
-);
+    );
 
 INSERT INTO
     roles (name, description, level)
-    VALUE (
-    'moderator',
-     'A moderator can update other users posts',
-    2
-);
+VALUES (
+           'user',
+           'A user can create post and comments',
+           1
+       );
 
 INSERT INTO
     roles (name, description, level)
-    VALUE (
-    'admin',
-     'An admin can update and delete other users posts',
-    3
-);
+VALUES (
+           'moderator',
+           'A moderator can update other users posts',
+           2
+       );
+
+INSERT INTO
+    roles (name, description, level)
+VALUES (
+           'admin',
+           'An admin can update and delete other users posts',
+           3
+       );
